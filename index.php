3412d3c1eae5b400b1582fd7d2b64954f1e5bf76
amarrak <68420701+amarrak@users.noreply.github.com>
2020-07-17 16:42:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-phpinfo();
+if (strpos($_SERVER["REQUEST_URI"], "/health/") === 0)
+{
+	echo '{"status": "OK"}';
+	die();
+}
+
+if (strpos($_SERVER["REQUEST_URI"], "/phpinfo/") === 0)
+{
+	phpinfo();
+	die();
+}
+
+echo "OK";
